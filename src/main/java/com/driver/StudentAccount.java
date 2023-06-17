b883f51425e2b9aa94d050fd85dff6ac5b093928
Shlok <theshlk77@gmail.com>
2023-06-17 19:05:19 +0530

    Added solution

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -6,7 +6,17 @@ public class StudentAccount extends BankAccount{
 
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
-
+        this.setMinBalance(0.0);
+        this.setName(name);
+        this.setBalance(balance);
+        this.institutionName=institutionName;
     }
 
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
 }
